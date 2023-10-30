42437c531789ba28618b5e861c576f761c59b7d0
samira hossain <samira.hossain@outlook.com>
2023-10-30 16:14:11 +1000

    form unordered list created

--- src/components/forms/EnquiryForm.js
+++ src/components/forms/EnquiryForm.js
@@ -35,47 +35,56 @@ const EnquiryForm = () => {
 
   return (
     <form>
-      {/* Input fields for Name and Email */}
-      <div>
-        <label>
-          Name
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <label>
-          Email
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </label>
-      </div>
-
-      {/* Select list for services */}
-      <select value={selectedService} onChange={(e) => setSelectedService(e.target.value)}>
-        <option disabled value="">-- Choose an option --</option>
-        {options.map((option, id) => (
-          <option value={option} key={`${id}-${option}`}>
-            {option}
-          </option>
-        ))}
-      </select>
-
-      {/* Textarea for message */}
-      <label>Message</label>
-      <textarea cols="40" rows="5" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-
-      {/* Submit button */}
-      <button type="button" onClick={handleSubmit}>
-        Submit
-      </button>
-
-      {/* Form messaging based on form submission and validation */}
-      {isSubmitted && isValid && (
-        <FormMessaging isError={false} content="Thanks for your details, we'll be in touch!" />
-      )}
-      {isSubmitted && !isValid && (
-        <FormMessaging isError content="Oops, something went wrong, please try again later." />
-      )}
-      {!isSubmitted && !isValid && (
-        <FormMessaging isError content="Please fill in the form" />
-      )}
+      <ul style={{ listStyleType: 'none', padding: 0 }}> {/* Style to remove default list bullets */}
+        <li>
+          <label>
+            Name
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+        </li>
+        <li>
+          <label>
+            Email
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </label>
+        </li>
+        <li>
+          <label>
+            Select a Service
+            <select value={selectedService} onChange={(e) => setSelectedService(e.target.value)}>
+              <option disabled value="">-- Choose an option --</option>
+              {options.map((option, id) => (
+                <option value={option} key={`${id}-${option}`}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </label>
+        </li>
+        <li>
+          <label>
+            Message
+            <textarea cols="40" rows="5" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+          </label>
+        </li>
+        <li>
+          <button type="button" onClick={handleSubmit}>
+            Submit
+          </button>
+        </li>
+        {/* Form messaging based on form submission and validation */}
+        <li>
+          {isSubmitted && isValid && (
+            <FormMessaging isError={false} content="Thanks for your details, we'll be in touch!" />
+          )}
+          {isSubmitted && !isValid && (
+            <FormMessaging isError content="Oops, something went wrong, please try again later." />
+          )}
+          {!isSubmitted && !isValid && (
+            <FormMessaging isError content="Please fill in the form" />
+          )}
+        </li>
+      </ul>
     </form>
   );
 };
